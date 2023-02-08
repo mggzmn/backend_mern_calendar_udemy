ef9bc41c2fd67e3315177f6e85db5f23f3cef295
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-08 10:38:22 -0600

    Auth routes

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const app = express();
 //directorio público
 app.use(express.static('public'));
 
-/* app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-}) */
-
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
     console.log('server')
