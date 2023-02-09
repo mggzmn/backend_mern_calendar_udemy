eebecd5c6867760bd6ca96f6d00f3c272acc490a
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-09 10:59:42 -0600

    394 validate fields

--- routes/events.js
+++ routes/events.js
@@ -5,14 +5,23 @@
     /api/events
 */
 const { Router } = require('express');
+const { check } = require('express-validator')
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 
 const router = Router();
 router.use(validarJWT);
 
 router.get('/', getEventos);
-router.post('/', crearEvento);
+router.post('/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio obligatoria').custom(isDate),
+        validarCampos
+    ]
+    , crearEvento);
 router.put('/:id', actualizarEvento);
 router.delete('/:id', eliminarEvento);
 
