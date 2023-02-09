cff3a672aaf6cb19bc098ffb1c2ed90022566980
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-09 11:17:36 -0600

    395 save event db

--- controllers/events.js
+++ controllers/events.js
@@ -1,26 +1,38 @@
 const { response } = require('express');
+const Evento = require("../models/Evento");
 const getEventos = async (req, res = response) => {
-   
+
     res.json({
         ok: true,
     })
 }
 
 const crearEvento = async (req, res = response) => {
-   
-    res.json({
-        ok: true,
-    })
+
+    try {
+        const evento = new Evento(req.body);
+        evento.user = req.uid;
+        const eventoDB = await evento.save();
+        res.json({
+            evento: eventoDB
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 const actualizarEvento = async (req, res = response) => {
-   
+
     res.json({
         ok: true,
     })
 }
 const eliminarEvento = async (req, res = response) => {
-   
+
     res.json({
         ok: true,
     })
