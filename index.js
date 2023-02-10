419f4fc2b885b16b48eabbfb7ca11dbb590285bd
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-10 15:45:42 -0600

    update routes

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 //rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
-
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 app.listen(process.env.PORT, () => {
     console.log('server')
